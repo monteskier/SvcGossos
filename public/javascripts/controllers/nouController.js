5b5fdfe78dd91c5645604d6de7194826d534d514
monteskier <jmontoliu7@gmail.com>
2017-04-01 08:51:41 +0200

    Express mongo and Angular

--- public/javascripts/controllers/nouController.js
+++ public/javascripts/controllers/nouController.js
@@ -1,5 +1,7 @@
 angular.module('Gossos')
   .controller("NouController",['$rootScope','$scope','$location', function($scope, $location){
     'use strict';
-
+    $scope.desar = function(gos){
+      console.log(gos);
+    }
   }]);
