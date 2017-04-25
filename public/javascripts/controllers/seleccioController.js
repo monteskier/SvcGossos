e500b73cb15f88df0f88a4fe373c9394c525aac8
Joaquim Montoliu <jmontoliu7@gmail.com>
2017-04-25 14:18:12 +0200

    El canvi del css3 en el documente de impressio ja no mostra cap element extra

--- public/javascripts/controllers/seleccioController.js
+++ public/javascripts/controllers/seleccioController.js
@@ -6,9 +6,12 @@ angular.module('Gossos')
       $location.path('/'+opcio);
     };
     $scope.print = function(card){
-      var innerContents = document.getElementById(card).innerHTML;
+      /*var innerContents = document.getElementById(card).innerHTML;
       var popupWindow = window.open('','_blank','width:600, height:400');
       popupWindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" /><link rel="stylesheet" type="text/css" href="stylesheets/print.css" /></head><body onload="window.print()">' + innerContents + '</body></html>');
       popupWindow.document.close();
+      */
+      window.print();
+      window.close();
     };
   }]);
